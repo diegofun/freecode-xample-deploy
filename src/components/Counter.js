bceb39f9690ee8faf5d5d6cca86c279fac5e7ae0
diego uribe <diegofun3@gmail.com>
2023-03-31 16:23:44 -0500

    after change click ocunter to react class component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,37 +1,42 @@
 import React from "react";
-import { useState } from "react";
 import Button from "./Button";
 import "../style-sheets/Button.css";
 import "../style-sheets/Counter.css";
 
-function Counter(){
-
-    const [numClicks, setNumClicks] = useState(0);
-
-    const handleClick=() => {
-        setNumClicks(numClicks + 1);
+class Counter extends React.Component {
+    constructor(){
+        super();
+        this.state = { numClicks:0 };
+        this.handleClick = this.handleClick.bind(this);
+        this.resetCounter = this.resetCounter.bind(this);
     }
 
-    const resetCounter=() => {
-        setNumClicks(0);
+    handleClick() {
+        this.setState(({ numClicks }) => ({ numClicks : numClicks + 1 }));
     }
-    
-    return(
-        <div className='contenedor-counter'>
-            <h1 className="page-title">Click counter</h1>
-            <div className="counter">
-                {numClicks}
+
+    resetCounter() {
+        this.setState({ numClicks: 0 });
+    }
+
+    render(){
+        return(
+            <div className='contenedor-counter'>
+                <h1 className="page-title">Click counter</h1>
+                <div className="counter">
+                    {this.state.numClicks}
+                </div>
+                <Button
+                    texto='Click'
+                    isClick={true}
+                    clickHandler={this.handleClick}/>
+                <Button 
+                    texto='Reset'
+                    isClick={false}
+                    clickHandler={this.resetCounter}/>
             </div>
-            <Button
-                texto='Click'
-                isClick={true}
-                clickHandler={handleClick}/>
-            <Button 
-                texto='Reset'
-                isClick={false}
-                clickHandler={resetCounter}/>
-        </div>
-    );
+        );
+    }
 }
 
 export default Counter;
